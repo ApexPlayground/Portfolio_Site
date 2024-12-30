11b9357e6e7e2500e2c2bf0fc9f8d1bca1c9e0f9
Divine <115173705+ApexPlayground@users.noreply.github.com>
2024-12-30 19:07:29 +0000

    update: not working on docflow again

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,11 +5,6 @@ const Project = () => {
             <h1 className="text-center text-5xl font-light">Projects</h1>
             <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5">
 
-                <div className="text-left space-y-2 border-2 hover:scale-105 transition-all duration-200 cursor-pointer px-8 py-10 border-black rounded-lg hover:bg-black hover:text-white">
-                    <h1 className="text-3xl font-semibold">DocFlow</h1>
-                    <h3 className="text-xl ">NextJS</h3>
-                    <p className="text-sm ">In progress</p>
-                </div>
 
 
                 <a href="https://github.com/ApexPlayground/Apex_Healthcare" className="text-left space-y-2 border-2 hover:scale-105 transition-all duration-200 cursor-pointer px-8 py-10 border-black rounded-lg hover:bg-black hover:text-white">
